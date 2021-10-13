c579a33e8a0dd018290a046c629db47fe430c9fd
akuzo <andrijkuzik@gmail.com>
2021-10-14 02:01:01 +0300

    Init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module project-profile
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+/*
+* The project idea: write a CLI tool for creating files profile.
+* Loop over all folders and files, collect lengths of each file lines
+* write report with average lines
+ */
+func main() {
+
+}
